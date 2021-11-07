aab6fa44d6bae109943f61d69a72a4baef4bafee
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2021-11-07 10:28:32 -0500

    Refactor routing logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,45 @@
 import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectIsAuthenticated } from "./modules/authentication/authenticationSlice";
 import TopBar from "./components/TopBar";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Surveys from "./pages/Surveys";
 
 export default function App() {
-  return (
-    <React.Fragment>
-      <BrowserRouter>
-        <TopBar />
+  const isAuthenticated = useSelector(selectIsAuthenticated);
 
-        <Routes>
-          <Route path="home" element={<Home />} />
-          <Route path="" element={<Navigate replace to="home" />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="signin" element={<Signin />} />
-        </Routes>
-      </BrowserRouter>
-    </React.Fragment>
+  return (
+    <BrowserRouter>
+      <TopBar />
+
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            isAuthenticated ? <Navigate replace to="/surveys" /> : <Home />
+          }
+        />
+        <Route path="" element={<Navigate replace to="/home" />} />
+
+        <Route
+          path="/signup"
+          element={
+            isAuthenticated ? <Navigate replace to="/surveys" /> : <Signup />
+          }
+        />
+
+        <Route path="/signin" element={<Signin />} />
+
+        <Route
+          path="/surveys"
+          element={
+            isAuthenticated ? <Surveys /> : <Navigate replace to="/signup" />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -4,12 +4,27 @@ import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 // import Box from "@mui/system/Box";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Navigate } from "react-router-dom";
+import {
+  selectIsAuthenticated,
+  signout,
+} from "../modules/authentication/authenticationSlice";
 
 export default function TopBar() {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  const onSignout = (
+    e: React.MouseEvent<HTMLButtonElement> | React.TouchEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    dispatch(signout());
+  };
+
   return (
-    <AppBar position="sticky" color="default">
+    <AppBar position="sticky" color="default" sx={{ marginBottom: 2 }}>
       <Toolbar sx={{ columnGap: "0.5em" }}>
         <Typography variant="h5">
           <Link to="home">Voting App</Link>
@@ -17,12 +32,25 @@ export default function TopBar() {
 
         <Box sx={{ flexGrow: 1 }} />
 
-        <Button color="primary">
-          <Link to="signin">Sign in</Link>
-        </Button>
-        <Button variant="contained" color="primary">
-          <Link to="signup">Sign up</Link>
-        </Button>
+        {isAuthenticated ? (
+          <React.Fragment>
+            <Button variant="contained" color="primary">
+              <Link to="surveys">Surveys</Link>
+            </Button>
+            <Button color="primary" onClick={onSignout}>
+              Sign out
+            </Button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Button color="primary">
+              <Link to="signin">Sign in</Link>
+            </Button>
+            <Button variant="contained" color="primary">
+              <Link to="signup">Sign up</Link>
+            </Button>
+          </React.Fragment>
+        )}
       </Toolbar>
     </AppBar>
   );

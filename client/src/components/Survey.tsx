732304cc55ae6300946de18e3931715f6bf89655
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2021-11-06 22:57:00 -0400

    Create Survey component

---
+++ client/src/components/Survey.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Survey() {
+  const [title, setTitle] = React.useState("");
+  const [questions, setQuestions] = React.useState([]);
+
+  return (
+    <div>
+      <h1>Survey</h1>
+    </div>
+  );
+}

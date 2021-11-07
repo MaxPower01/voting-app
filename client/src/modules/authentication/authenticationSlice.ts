adba4a20d797d9a871e4c865e1e01b481d4f60bf
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2021-11-07 10:26:28 -0500

    Refactor

--- client/src/modules/authentication/authenticationSlice.ts
+++ client/src/modules/authentication/authenticationSlice.ts
@@ -61,7 +61,7 @@ export const { signin, signout } = authenticationSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectAuthenticationState = (state: RootState) =>
+export const selectIsAuthenticated = (state: RootState) =>
   state.authentication.isAuthenticated;
 
 export default authenticationSlice.reducer;
